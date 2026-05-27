0792a3c16a3958ae095af87bdac5d20f2d8b7cb0
nizsz <PohaDeCla@gmail.com>
2026-05-27 20:17:30 -0300

    Creating assets folders Inserting all assets Creating Menu: background and font Creating the const file

--- code/game.py
+++ code/game.py
@@ -1,24 +1,18 @@
 import pygame
-
 from code.menu import Menu
-
+from code.const import WIN_WINDTH,WIN_HEIGHT
 
 class Game:
     """Classe principal"""
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode((600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WINDTH, WIN_HEIGHT))
 
-    def run(self) -> None:
+    def run(self):
 
         while True:
             menu = Menu(self.window)
             menu.run()
 
             pass
-            # Check for all events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()  # close game
-                    quit()  # end game
 
--- code/menu.py
+++ code/menu.py
@@ -1,10 +1,37 @@
 import pygame
-from game import Game
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.const import WIN_WINDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+
 
 class Menu:
     """Menu inicial. Composição forte com Game (+own)."""
+
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('./assets/BackgroundMenu.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
-    def run(self) -> None:
-        self.game.run()
+    def run(self):
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "Ship", COLOR_ORANGE, ((WIN_WINDTH / 2), 70))
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WINDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WINDTH / 2), 200 + 25 * i))
+
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # close game
+                    quit()  # end game
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
