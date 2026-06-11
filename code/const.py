42224f1c555b055ece11a00ee0dd52d2da50b00b
nizsz <PohaDeCla@gmail.com>
2026-06-11 18:52:54 -0300

    SQLITE 3 Implementation for Score using proxy design pattern

--- code/const.py
+++ code/const.py
@@ -6,6 +6,7 @@ C_WHITE = (255, 255, 255)
 C_SELECTION = (255, 255, 0)
 C_GREEN = (0, 128, 0)
 C_CYAN = (0, 128, 128)
+C_YELLOW = (255, 255, 0)
 
 #E
 
@@ -135,11 +136,30 @@ PLAYER_KEY_SHOOT = {
 
 SPAWN_TIME = 2000
 
-# W
 
 #T
 TIMEOUT_LEVEL = 20000
 TIMEOUT_STEP = 100
 
+# W
 WIN_WIDTH= 576
 WIN_HEIGHT= 324
+
+# S
+
+SCORE_POS = {
+    'Title': (WIN_WIDTH / 2, 50),
+    'EnterName': (WIN_WIDTH / 2, 80),
+    'Label': (WIN_WIDTH / 2, 90),
+    'Name': (WIN_WIDTH / 2, 110),
+    0: (WIN_WIDTH / 2, 110),
+    1: (WIN_WIDTH / 2, 130),
+    2: (WIN_WIDTH / 2, 150),
+    3: (WIN_WIDTH / 2, 170),
+    4: (WIN_WIDTH / 2, 190),
+    5: (WIN_WIDTH / 2, 210),
+    6: (WIN_WIDTH / 2, 230),
+    7: (WIN_WIDTH / 2, 250),
+    8: (WIN_WIDTH / 2, 270),
+    9: (WIN_WIDTH / 2, 290),
+}
--- code/game.py
+++ code/game.py
@@ -3,6 +3,7 @@ import pygame
 from code.level import Level
 from code.menu import Menu
 from code.const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.score import Score
 
 
 class Game:
@@ -11,8 +12,8 @@ class Game:
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
-
         while True:
+            score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()
 
@@ -23,7 +24,11 @@ class Game:
                 if level_return:
                     level = Level(self.window, "Level2", menu_return, player_score)
                     level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
 
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit() #close window
                 quit() #end pygame
